b1f66b4101fc44e750cebf3e7499a4a47030f3fc
Dzmitry <ignatovich.dm@gmail.com>
2024-10-07 18:20:22 +0200

    tesing new path

--- src/InvestorComponent.tsx
+++ src/InvestorComponent.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 
 const InvestorComponent = () => {
     return (
-        <Box p={5} shadow="md" borderWidth="1px">
+        <Box p={5} mt={10} shadow="md" borderWidth="1px">
             <Heading fontSize="xl">Investor Information</Heading>
             <Text mt={4}>
                 This section contains important information for investors. Stay updated with the latest financial news and analysis.
